e3abd7c4979ece908045ea040e99b72f5e8e8f2d
Andrew Li <qwerty526526@gmail.com>
2018-10-04 12:37:31 -0400

    First Commit
    Project Skeleton

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+    public int speed;
+    private Camera mycam;
+    private bool seatLocked;
+    private SmoothMouseLook mouseLook;
+	// Use this for initialization
+	void Start () {
+        mycam = GetComponent<Camera>();
+        mouseLook = GetComponent<SmoothMouseLook>();
+        mouseLook.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = mycam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("You selected the " + hit.transform.name);
+                if (hit.collider.gameObject.tag == "Chair" && !seatLocked) {
+                    //Screen.lockCursor = true;
+                    mycam.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y+1, hit.transform.position.z);
+                    mycam.transform.eulerAngles = new Vector3(0, 0, 0);
+                    mycam.fieldOfView = 90;
+                    seatLocked = false;
+                    mouseLook.enabled = true;
+                }
+                // the object identified by hit.transform was clicked
+                // do whatever you want
+            }           
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ Assets/Scripts/ChairPrefab.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChairPrefab : MonoBehaviour
+{
+    private Camera mainCam;
+
+	// Use this for initialization
+	void Start () {
+        mainCam = Object.FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log(this.gameObject.name + " Was Clicked.");
+        mainCam.transform.position = new Vector3(transform.position.x, transform.position.y+1, transform.position.z);
+        mainCam.transform.eulerAngles= new Vector3(0, 0, 0);
+        mainCam.fieldOfView = 90;
+    }
+}
